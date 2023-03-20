5c355dea9df49df505b87a7c822f4325a697431a
nmarforio <n.marforio@gmail.com>
2023-03-20 16:37:35 +0100

    only google singin

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,8 @@
 import GlobalStyle from "../styles/styles";
 import Title from "@/components/Title";
 import { SessionProvider } from "next-auth/react";
-import NavBar from "@/components/NavBar";
+
+import Layout from "@/components/Layout";
 
 export default function App({
   Component,
@@ -9,11 +10,12 @@ export default function App({
 }) {
   return (
     <>
-      <Title />
-      <GlobalStyle />
       <SessionProvider session={session}>
-        <NavBar />
-        <Component {...pageProps} />
+        <Title />
+        <Layout>
+          <GlobalStyle />
+          <Component {...pageProps} />
+        </Layout>
       </SessionProvider>
     </>
   );
--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,5 +1,4 @@
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import clientPromise from "@/db/mongodbconnect";
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
